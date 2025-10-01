7aa7c1c9aba5fceacaead1032089b841f1c1dca5
Levon24 <levon24@mail.ru>
2025-10-01 18:25:21 +0400

    prototype

--- User/main.c
+++ User/main.c
@@ -1,104 +1,51 @@
-/********************************** (C) COPYRIGHT *******************************
- * File Name          : main.c
- * Author             : WCH
- * Version            : V1.0.0
- * Date               : 2023/12/25
- * Description        : Main program body.
- *********************************************************************************
- * Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
- * Attention: This software (modified or not) and binary are used for 
- * microcontroller manufactured by Nanjing Qinheng Microelectronics.
- *******************************************************************************/
-
-/*
- *@Note
- *Multiprocessor communication mode routine:
- *Master:USART1_Tx(PD5)\USART1_Rx(PD6).
- *This routine demonstrates that USART1 receives the data sent by CH341 and inverts
- *it and sends it (baud rate 115200).
- *
- *Hardware connection:PD5 -- Rx
- *                     PD6 -- Tx
- *
- */
-
 #include "debug.h"
 
+#define BUTTON_SETTINGS GPIO_Pin_3
+#define BUTTON_NEXT     GPIO_Pin_4
+#define BUTTON_UP       GPIO_Pin_5
+#define BUTTON_DOWN     GPIO_Pin_6
+#define BUTTON_PRESSED  0
 
 /* Global define */
 
-
 /* Global Variable */
-vu8 val;
 
-/*********************************************************************
- * @fn      USARTx_CFG
- *
- * @brief   Initializes the USART2 & USART3 peripheral.
- *
- * @return  none
- */
-void USARTx_CFG(void)
-{
-    GPIO_InitTypeDef  GPIO_InitStructure = {0};
-    USART_InitTypeDef USART_InitStructure = {0};
+/*******************************************************************/
+void initPortC() {
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD | RCC_APB2Periph_USART1, ENABLE);
-
-    /* USART1 TX-->D.5   RX-->D.6 */
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
-    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_30MHz;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
-    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
-    GPIO_Init(GPIOD, &GPIO_InitStructure);
-
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
-
-    USART_Init(USART1, &USART_InitStructure);
-    USART_Cmd(USART1, ENABLE);
+  GPIO_InitTypeDef initButtons = {0};
+  initButtons.GPIO_Pin = BUTTON_SETTINGS | BUTTON_NEXT | BUTTON_UP | BUTTON_DOWN;
+  initButtons.GPIO_Mode = GPIO_Mode_IPU;
+  initButtons.GPIO_Speed = GPIO_Speed_30MHz;
+  GPIO_Init(GPIOC, &initButtons);
 }
 
 /*********************************************************************
  * @fn      main
- *
  * @brief   Main program.
- *
  * @return  none
  */
-int main(void)
-{
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
-    SystemCoreClockUpdate();
-    Delay_Init();
+int main(void) {
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_1);
+  SystemCoreClockUpdate();
+  Delay_Init();
 #if (SDI_PRINT == SDI_PR_OPEN)
-    SDI_Printf_Enable();
+  SDI_Printf_Enable();
 #else
-    USART_Printf_Init(115200);
+  USART_Printf_Init(115200);
 #endif
-    printf("SystemClk:%d\r\n",SystemCoreClock);
-    printf( "ChipID:%08x\r\n", DBGMCU_GetCHIPID() );
+  printf("SystemClk: %d\r\n", SystemCoreClock);
+  printf("ChipID: %08x\r\n", DBGMCU_GetCHIPID());
 
-    USARTx_CFG();
+  initPortC();
 
-    while(1)
-    {
-
-        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET)
-        {
-            /* waiting for receiving finish */
-        }
-        val = (USART_ReceiveData(USART1));
-        USART_SendData(USART1, ~val);
-        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET)
-        {
-            /* waiting for sending finish */
-        }
+  while(1) {
+    uint16_t buttons = GPIO_ReadInputData(GPIOC);
+    if ((buttons & BUTTON_SETTINGS) == BUTTON_PRESSED) {
+      printf("Settings pressed");
     }
+
+    Delay_Ms(20);
+  }
 }
